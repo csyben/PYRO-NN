6aa79b1785272db2f80dd62478ed165e404a84e9
Markus Michen <markus.michen@fau.de>
2019-01-16 22:48:55 +0100

    [added] - parker weights

--- deep_ct_reconstruction/ct_reconstruction/helpers/filters/weights.py
+++ deep_ct_reconstruction/ct_reconstruction/helpers/filters/weights.py
@@ -19,5 +19,27 @@ def cosine_weights_3d(geometry):
 
     return w
 
+# parker weights
+def parker_weights_2d(geometry):
+    weights = np.zeros(geometry.sinogram_shape)
 
-#
+    fan_angle = np.tan(geometry.detector_shape[0] * geometry.detector_spacing[0] / 2.0 / geometry.soure_detector_distance)
+
+    gamma_range = np.linspace(-fan_angle, fan_angle, geometry.detector_shape[0], endpoint=False)
+    beta_range = np.linspace(0, geometry.angular_range, geometry.number_of_projections, endpoint=False)
+
+    for i in range(weights.shape[0]):
+        for j in range(weights.shape[1]):
+            weights[i, j] = parker_weights_fct(beta_range[j], gamma_range[i], fan_angle)
+
+    return weights
+
+def parker_weights_fct(beta, gamma, fan_angle):
+    if 0 <= beta and beta < 2 * (fan_angle + gamma):
+        return np.sin((np.pi / 4.0) * beta / (fan_angle + gamma)) ** 2
+    elif np.pi + 2 * gamma <= beta and beta <= np.pi + 2 * fan_angle:
+        return np.sin((np.pi / 4.0) * (2 * fan_angle + np.pi - beta) / (fan_angle - gamma)) ** 2
+    elif beta >= np.pi + 2 * fan_angle:
+        return 0.0
+
+    return 1.0
