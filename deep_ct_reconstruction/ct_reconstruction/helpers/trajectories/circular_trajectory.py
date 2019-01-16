e0df5482f1eac8594f44e770e75039a6c0c8f634
Markus Michen <markus.michen@fau.de>
2019-01-16 17:57:47 +0100

    [added] - own (CURRENTLY WRONG!) circular_trajectory function

--- deep_ct_reconstruction/ct_reconstruction/helpers/trajectories/circular_trajectory.py
+++ deep_ct_reconstruction/ct_reconstruction/helpers/trajectories/circular_trajectory.py
@@ -1,5 +1,5 @@
 import numpy as np
-import pyconrad as pyc  # TODO: get independent of pyconrad
+import pyconrad as pyc
 
 
 def circular_trajectory_2d(geometry):
@@ -17,7 +17,7 @@ def circular_trajectory_2d(geometry):
     return rays
 
 
-def circular_trajectory_3d(geometry): # TODO: still have to get independent of pyconrad
+def circular_trajectory_3d_pyconrad(geometry): # TODO: still have to get independent of pyconrad
     """
         Generates the projection matrices defining a circular trajectory for use with the 3d projection layers.
     Args:
@@ -66,3 +66,80 @@ def circular_trajectory_3d(geometry): # TODO: still have to get independent of p
         _projection_matrix[proj] = circ_traj.getProjectionMatrix(proj).computeP().as_numpy()
 
     return _projection_matrix
+
+
+def circular_trajectory_3d(geometry):
+    """
+        Generates the projection matrices defining a circular trajectory for use with the 3d projection layers.
+    Args:
+        geometry: 3d Geometry class including angular_range, number_of_projections, source_detector_distance,
+        detector_shape, detector_spacing, volume_origin, volume_shape and volume_spacing.
+    Returns:
+        Projection matrices with shape (num_projections, 3, 4) as np.array.
+    """
+
+    #TODO: not working currently!
+    # conrad docu:
+    # https://www5.cs.fau.de/conrad/doc/edu/stanford/rsl/conrad/geometry/Projection.html
+
+    projection_matrices = np.zeros((geometry.number_of_projections, 3, 4))
+
+    # configure intrinsic camera parameters
+    intrinsic_params_mat = np.eye(3, 3)
+    for i in range(2):
+        intrinsic_params_mat[i, i] = geometry.detector_spacing[1-i] * geometry.source_detector_distance
+        intrinsic_params_mat[i, 2] = geometry.detector_origin[1-i]
+
+    # configure projection
+    projection = np.zeros((3, 4))
+    for i in range(3):
+        projection[i, i] = 1.0
+
+    # configure extrinisc pararams and create projection_matrices
+    current_angle = 0.0
+    angular_increment = geometry.angular_range/geometry.number_of_projections
+    for p in range(geometry.number_of_projections):
+        # extrinsic rotation matrix about z-axis + translation x,y with source_isocenter_dist
+        extrinsic_params_mat = np.eye(4, 4)
+        extrinsic_params_mat[0, 0] = np.cos(current_angle)
+        extrinsic_params_mat[1, 1] = extrinsic_params_mat[0, 0]
+        extrinsic_params_mat[0, 1] = -np.sin(current_angle)
+        extrinsic_params_mat[1, 0] = extrinsic_params_mat[0, 1]
+        extrinsic_params_mat[0, 3] = geometry.source_isocenter_distance
+        extrinsic_params_mat[1, 3] = geometry.source_isocenter_distance
+
+        # calculate projection matrix
+        projection_matrices[p] = np.dot(np.dot(intrinsic_params_mat, projection), extrinsic_params_mat)
+
+        # next angle
+        current_angle += angular_increment
+
+    return projection_matrices
+
+from deep_ct_reconstruction.ct_reconstruction.geometry.geometry_cone_3d import GeometryCone3D
+
+# TEST CODE! to be removed later on
+if __name__ == '__main__':
+    # ------------------ Declare Parameters ------------------
+
+    # Volume Parameters:
+    volume_size = 100
+    volume_shape = [volume_size, volume_size, volume_size]
+    volume_spacing = [0.5, 0.5, 0.5]
+
+    # Detector Parameters:
+    detector_shape = [2*volume_size, 2*volume_size]
+    detector_spacing = [0.5, 0.5]
+
+    # Trajectory Parameters:
+    number_of_projections = 360
+    angular_range = 2 * np.pi
+
+    source_detector_distance = 200
+    source_isocenter_distance = 200
+
+    # create Geometry class
+    geometry = GeometryCone3D(volume_shape, volume_spacing, detector_shape, detector_spacing, number_of_projections, angular_range, source_detector_distance, source_isocenter_distance)
+    geometry.set_projection_matrices(circular_trajectory_3d_pyconrad(geometry))
+
+    circular_trajectory_3d(geometry)
