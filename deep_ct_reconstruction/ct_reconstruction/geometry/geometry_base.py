71cc231be2ee5f6b28c3b5f2d281f1963ec83bd7
Markus Michen <markus.michen@fau.de>
2019-01-16 20:37:01 +0100

    [modified] - shape members are no longer np.float32

--- deep_ct_reconstruction/ct_reconstruction/geometry/geometry_base.py
+++ deep_ct_reconstruction/ct_reconstruction/geometry/geometry_base.py
@@ -31,19 +31,19 @@ class GeometryBase:
         self.np_dtype = np.float32  # datatype for np.arrays make sure everything will be float32
 
         # Volume Parameters:
-        self.volume_shape = np.array(volume_shape, dtype=self.np_dtype)
+        self.volume_shape = np.array(volume_shape)
         self.volume_spacing = np.array(volume_spacing, dtype=self.np_dtype)
         self.volume_origin = -(self.volume_shape - 1) / 2.0 * self.volume_spacing
 
         # Detector Parameters:
-        self.detector_shape = np.array(detector_shape, dtype=self.np_dtype)
+        self.detector_shape = np.array(detector_shape)
         self.detector_spacing = np.array(detector_spacing, dtype=self.np_dtype)
         self.detector_origin = -(self.detector_shape - 1) / 2.0 * self.detector_spacing
 
         # Trajectory Parameters:
         self.number_of_projections = number_of_projections
         self.angular_range = angular_range
-        self.sinogram_shape = np.array([self.number_of_projections, *self.detector_shape], dtype=self.np_dtype)
+        self.sinogram_shape = np.array([self.number_of_projections, *self.detector_shape])
 
         self.source_detector_distance = source_detector_distance
         self.source_isocenter_distance = source_isocenter_distance
