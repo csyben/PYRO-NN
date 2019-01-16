55dd0c2687d32f23eb32b4c70a7ad209a69f8976
Markus Michen <markus.michen@fau.de>
2019-01-16 18:02:09 +0100

    [modified] - updated generate_reco/sinogram functions

--- deep_ct_reconstruction/ct_reconstruction/helpers/misc/generate_reco.py
+++ deep_ct_reconstruction/ct_reconstruction/helpers/misc/generate_reco.py
@@ -1,23 +1,23 @@
 import lme_custom_ops
 
 
-def generate_reco(sinogram, layer, geometry_params_dict):
-    result = layer(sinogram, geometry_params_dict)
+def generate_reco(sinogram, layer, geometry):
+    result = layer(sinogram, geometry)
     sinogram = result.eval()
     return sinogram
 
 def generate_reco_parallel_2d(phantom, geometry):
-    result = lme_custom_ops.parallel_backprojection2d(phantom, *geometry.get_parallel_projection2d_params_dict())
+    result = lme_custom_ops.parallel_backprojection2d(phantom, geometry)
     sinogram = result.eval()
     return sinogram
 
 def generate_reco_fan_2d(phantom, geometry):
-    result = lme_custom_ops.fan_backprojection2d(phantom, *geometry.get_fan_projection2d_params_dict())
+    result = lme_custom_ops.fan_backprojection2d(phantom, geometry)
     sinogram = result.eval()
     return sinogram
 
 def generate_reco_cone_3d(phantom, geometry):
-    result = lme_custom_ops.cone_backprojection3d(phantom, *geometry.get_cone_projection3d_params_dict())
+    result = lme_custom_ops.cone_backprojection3d(phantom, geometry)
     sinogram = result.eval()
     return sinogram
 
--- deep_ct_reconstruction/ct_reconstruction/helpers/misc/generate_sinogram.py
+++ deep_ct_reconstruction/ct_reconstruction/helpers/misc/generate_sinogram.py
@@ -1,22 +1,22 @@
 import lme_custom_ops
 
 
-def generate_sinogram(phantom, layer, geometry_params_dict):
-    result = layer(phantom, geometry_params_dict)
+def generate_sinogram(phantom, layer, geometry):
+    result = layer(phantom, geometry)
     sinogram = result.eval()
     return sinogram
 
 def generate_sinogram_parallel_2d(phantom, geometry):
-    result = lme_custom_ops.parallel_projection2d(phantom, *geometry.get_parallel_projection2d_params_dict())
+    result = lme_custom_ops.parallel_projection2d(phantom, geometry)
     sinogram = result.eval()
     return sinogram
 
 def generate_sinogram_fan_2d(phantom, geometry):
-    result = lme_custom_ops.fan_projection2d(phantom, *geometry.get_fan_projection2d_params_dict())
+    result = lme_custom_ops.fan_projection2d(phantom, geometry)
     sinogram = result.eval()
     return sinogram
 
 def generate_sinogram_cone_3d(phantom, geometry):
-    result = lme_custom_ops.cone_projection3d(phantom, *geometry.get_cone_projection3d_params_dict())
+    result = lme_custom_ops.cone_projection3d(phantom, geometry)
     sinogram = result.eval()
     return sinogram
