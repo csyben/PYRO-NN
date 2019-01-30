f9ad4d8a53144e255e66f6a622c8bc71d1cda734
Markus Michen <markus.michen@fau.de>
2019-01-30 12:41:10 +0100

    [added] - SetTensorProtoProperty Decorator for setting of members. This will automatically set TensorProtos on assignment for the members that implement it.

--- deep_ct_reconstruction/ct_reconstruction/geometry/geometry_base.py
+++ deep_ct_reconstruction/ct_reconstruction/geometry/geometry_base.py
@@ -31,42 +31,76 @@ class GeometryBase:
         self.np_dtype = np.float32  # datatype for np.arrays make sure everything will be float32
 
         # Volume Parameters:
-        self.volume_shape = np.array(volume_shape)
+        self.volume_shape   = np.array(volume_shape)
         self.volume_spacing = np.array(volume_spacing, dtype=self.np_dtype)
-        self.volume_origin = -(self.volume_shape - 1) / 2.0 * self.volume_spacing
+        self.volume_origin  = -(self.volume_shape - 1) / 2.0 * self.volume_spacing
 
         # Detector Parameters:
-        self.detector_shape = np.array(detector_shape)
+        self.detector_shape   = np.array(detector_shape)
         self.detector_spacing = np.array(detector_spacing, dtype=self.np_dtype)
-        self.detector_origin = -(self.detector_shape - 1) / 2.0 * self.detector_spacing
+        self.detector_origin  = -(self.detector_shape - 1) / 2.0 * self.detector_spacing
 
         # Trajectory Parameters:
         self.number_of_projections = number_of_projections
-        self.angular_range = angular_range
-        self.sinogram_shape = np.array([self.number_of_projections, *self.detector_shape])
+        self.angular_range         = angular_range
+        self.sinogram_shape        = np.array([self.number_of_projections, *self.detector_shape])
 
-        self.source_detector_distance = source_detector_distance
+        self.source_detector_distance  = source_detector_distance
         self.source_isocenter_distance = source_isocenter_distance
-
-        # Tensor Protos:
-        self.init_tensor_proto_members()
-
-    def init_tensor_proto_members(self):
-        """
-            This function inits all members again as tensor_proto members for the layer calls.
-        """
-        self.tensor_proto_volume_shape = self.to_tensor_proto(self.volume_shape)
-        self.tensor_proto_volume_spacing = self.to_tensor_proto(self.volume_spacing)
-        self.tensor_proto_volume_origin = self.to_tensor_proto(self.volume_origin)
-
-        self.tensor_proto_detector_shape = self.to_tensor_proto(self.detector_shape)
-        self.tensor_proto_detector_spacing = self.to_tensor_proto(self.detector_spacing)
-        self.tensor_proto_detector_origin = self.to_tensor_proto(self.detector_origin)
-
-        self.tensor_proto_sinogram_shape = self.to_tensor_proto(self.sinogram_shape)
 
     def to_tensor_proto(self, member):
         """
             Wraps the TensorProto creation function.
         """
         return tf.contrib.util.make_tensor_proto(member, tf.float32)
+
+    class SetTensorProtoProperty(object):
+        """
+            A setter descriptor that only intercepts variable assignment.
+            Allows for setting of the corresponding TensorProto members.
+            Use @SetTensorProtoProperty for every member that also needs TensorProto members.
+        """
+
+        def __init__(self, func, doc=None):
+            self.func = func
+            self.__doc__ = doc if doc is not None else func.__doc__
+
+        def __set__(self, obj, value):
+            return self.func(obj, value)
+
+
+    @SetTensorProtoProperty
+    def volume_shape(self, value):
+        self.__dict__['volume_shape'] = value
+        self.tensor_proto_volume_shape = self.to_tensor_proto(self.volume_shape)
+
+    @SetTensorProtoProperty
+    def volume_spacing(self, value):
+        self.__dict__['volume_spacing'] = value
+        self.tensor_proto_volume_spacing = self.to_tensor_proto(self.volume_spacing)
+
+    @SetTensorProtoProperty
+    def volume_origin(self, value):
+        self.__dict__['volume_origin'] = value
+        self.tensor_proto_volume_origin = self.to_tensor_proto(self.volume_origin)
+
+    @SetTensorProtoProperty
+    def detector_shape(self, value):
+        self.__dict__['detector_shape'] = value
+        self.tensor_proto_detector_shape = self.to_tensor_proto(self.detector_shape)
+
+    @SetTensorProtoProperty
+    def detector_spacing(self, value):
+        self.__dict__['detector_spacing'] = value
+        self.tensor_proto_detector_spacing = self.to_tensor_proto(self.detector_spacing)
+
+    @SetTensorProtoProperty
+    def detector_origin(self, value):
+        self.__dict__['detector_origin'] = value
+        self.tensor_proto_detector_origin = self.to_tensor_proto(self.detector_origin)
+
+    @SetTensorProtoProperty
+    def sinogram_shape(self, value):
+        self.__dict__['sinogram_shape'] = value
+        self.tensor_proto_sinogram_shape = self.to_tensor_proto(self.sinogram_shape)
+
