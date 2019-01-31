2de997a8578c9615f133bf115f251c7d7584c491
Markus Michen <markus.michen@fau.de>
2019-01-31 11:11:37 +0100

    [added] - riess weights

--- deep_ct_reconstruction/ct_reconstruction/helpers/filters/weights.py
+++ deep_ct_reconstruction/ct_reconstruction/helpers/filters/weights.py
@@ -21,11 +21,21 @@ def cosine_weights_3d(geometry):
 
 # parker weights
 def parker_weights_2d(geometry):
+
+    def parker_weights_fct(beta, gamma, fan_angle):
+        if 0 <= beta and beta < 2 * (fan_angle + gamma):
+            return np.sin((np.pi / 4.0) * beta / (fan_angle + gamma)) ** 2
+        elif np.pi + 2 * gamma <= beta and beta <= np.pi + 2 * fan_angle:
+            return np.sin((np.pi / 4.0) * (2 * fan_angle + np.pi - beta) / (fan_angle - gamma)) ** 2
+        elif beta >= np.pi + 2 * fan_angle:
+            return 0.0
+        return 1.0
+
     weights = np.zeros(geometry.sinogram_shape)
 
-    fan_angle = np.tan(geometry.detector_shape[0] * geometry.detector_spacing[0] / 2.0 / geometry.soure_detector_distance)
+    fan_angle = geometry.fan_angle
 
-    gamma_range = np.linspace(-fan_angle, fan_angle, geometry.detector_shape[0], endpoint=False)
+    gamma_range = np.linspace(-fan_angle, fan_angle, geometry.detector_shape[1], endpoint=False)
     beta_range = np.linspace(0, geometry.angular_range, geometry.number_of_projections, endpoint=False)
 
     for i in range(weights.shape[0]):
@@ -34,11 +44,47 @@ def parker_weights_2d(geometry):
 
     return weights
 
-def parker_weights_fct(beta, gamma, fan_angle):
-    if 0 <= beta and beta < 2 * (fan_angle + gamma):
-        return np.sin((np.pi / 4.0) * beta / (fan_angle + gamma)) ** 2
-    elif np.pi + 2 * gamma <= beta and beta <= np.pi + 2 * fan_angle:
-        return np.sin((np.pi / 4.0) * (2 * fan_angle + np.pi - beta) / (fan_angle - gamma)) ** 2
-    elif beta >= np.pi + 2 * fan_angle:
-        return 0.0
-    return 1.0
+
+
+# riess weights
+def riess_weights_2d(geometry):
+
+    over = 2 * geometry.fan_angle
+
+    def w1(b, a):
+        x = np.pi + over - b
+        y = over - 2 * a
+        z = np.pi / 2 * (x / y)
+        return np.pow(np.sin(z), 2)
+
+    def w2(b, a):
+        x = b
+        y = over + 2 * a
+        z = np.pi / 2 * (x / y)
+        return np.pow(np.sin(z), 2)
+
+    def riess_weights_fct(beta, gamma, fan_angle):
+        if np.pi + 2 * fan_angle <= beta and beta <= np.pi + over:
+            return w1(beta, gamma)
+        elif np.pi + 2 * over - 2 * fan_angle <= beta and beta <= np.pi + over:
+            return 2 - w1(beta, gamma)
+        elif 0 <= beta and beta <= 2 * fan_angle + over:
+            return w2(beta, gamma)
+        elif 0 <= beta and beta <= -over - 2 * fan_angle:
+            return 2 - w2(beta, gamma)
+        else:
+            return 1
+
+    weights = np.zeros(geometry.sinogram_shape)
+
+    fan_angle = geometry.fan_angle
+
+    gamma_range = np.linspace(-fan_angle, fan_angle, geometry.detector_shape[1], endpoint=False)
+    beta_range = np.linspace(0, geometry.angular_range, geometry.number_of_projections, endpoint=False)
+
+    for i in range(weights.shape[0]):
+        for j in range(weights.shape[1]):
+            weights[i, j] = riess_weights_fct(beta_range[j], gamma_range[i], fan_angle)
+
+    return weights
+
