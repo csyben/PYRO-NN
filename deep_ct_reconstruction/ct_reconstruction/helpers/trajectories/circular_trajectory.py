c46c1d0d56fa72e0b364d069bee8b67bcc3f06d1
Markus Michen <markus.michen@fau.de>
2019-02-02 12:07:25 +0100

    [finally] - own circular trajectory function

--- deep_ct_reconstruction/ct_reconstruction/helpers/trajectories/circular_trajectory.py
+++ deep_ct_reconstruction/ct_reconstruction/helpers/trajectories/circular_trajectory.py
@@ -17,7 +17,7 @@ def circular_trajectory_2d(geometry):
     return rays
 
 
-def circular_trajectory_3d_pyconrad(geometry): # TODO: still have to get independent of pyconrad
+def circular_trajectory_3d_pyconrad(geometry):
     """
         Generates the projection matrices defining a circular trajectory for use with the 3d projection layers.
     Args:
@@ -39,8 +39,8 @@ def circular_trajectory_3d_pyconrad(geometry): # TODO: still have to get indepen
     circ_traj = _.CircularTrajectory()
     circ_traj.setSourceToDetectorDistance(geometry.source_detector_distance)
 
-    circ_traj.setPixelDimensionX(np.float64(geometry.detector_spacing[0]))
-    circ_traj.setPixelDimensionY(np.float64(geometry.detector_spacing[1]))
+    circ_traj.setPixelDimensionX(np.float64(geometry.detector_spacing[1]))
+    circ_traj.setPixelDimensionY(np.float64(geometry.detector_spacing[0]))
     circ_traj.setDetectorHeight(int(geometry.detector_shape[0]))
     circ_traj.setDetectorWidth(int(geometry.detector_shape[1]))
 
@@ -62,15 +62,10 @@ def circular_trajectory_3d_pyconrad(geometry): # TODO: still have to get indepen
     circ_traj.setTrajectory(geometry.number_of_projections, geometry.source_isocenter_distance, average_angular_increment,
                             detector_offset_u, detector_offset_v, DETECTORMOTION_MINUS, ROTATIONAXIS_MINUS, rotationAxis, center, 0)
 
-    conrad_Rt = np.zeros((geometry.number_of_projections, 4, 4))
-
     for proj in range(0, geometry.number_of_projections):
         _projection_matrix[proj] = circ_traj.getProjectionMatrix(proj).computeP().as_numpy()
-        conrad_Rt[proj] = circ_traj.getProjectionMatrix(proj).getRt().as_numpy()
 
-    conrad_intrinsic = circ_traj.getProjectionMatrix(0).getK().as_numpy()
-
-    return _projection_matrix, conrad_Rt, conrad_intrinsic
+    return _projection_matrix
 
 
 def circular_trajectory_3d(geometry):
@@ -85,18 +80,15 @@ def circular_trajectory_3d(geometry):
         Projection matrices with shape (num_projections, 3, 4) as np.array.
     """
 
-    #TODO: not working currently!
-    # conrad docu:
-    # https://www5.cs.fau.de/conrad/doc/edu/stanford/rsl/conrad/geometry/Projection.html
-
-    # empty
+    # init empty
     projection_matrices = np.zeros((geometry.number_of_projections, 3, 4))
 
+    # axes for later use
     x_axis = np.array([1.0, 0.0, 0.0])
     y_axis = np.array([0.0, 1.0, 0.0])
     z_axis = np.array([0.0, 0.0, 1.0])
 
-    # defining u and v directions by: DETECTORMOTION_MINUS and ROTATIONAXIS_MINUS
+    # defining u and v directions by: main coord axes
     u_dir =  y_axis
     v_dir = -x_axis
 
@@ -106,13 +98,8 @@ def circular_trajectory_3d(geometry):
         intrinsic_params_mat[i, i] = geometry.source_detector_distance / geometry.detector_spacing[1-i]
 
     # calc and set detector origin
-    intrinsic_params_mat[0:2, 2] = (geometry.detector_shape * geometry.detector_spacing)[::-1]
+    intrinsic_params_mat[0:2, 2] = (geometry.detector_shape * 0.5)[::-1]
 
-    diff_intrinsic = geometry.conrad_intrinsic - intrinsic_params_mat
-    #print("diff_intrinsic\n", diff_intrinsic)
-
-    # configure projection
-    projection = np.eye(3, 4)
 
     # configure extrinisc pararams and create projection_matrices
     current_angle = 0.0
@@ -122,24 +109,24 @@ def circular_trajectory_3d(geometry):
         # calculate extrinsic params
         extrinsic_params_mat = np.eye(4, 4)
 
-        # rotation of axes from world system to plane of rotation system PLANE_T_CENT
+        # rotation of axes from world system to plane of rotation system
         R_to_plane = np.eye(4, 4)
         R_to_plane[0:3, 0:3] = np.array([z_axis, np.cross(z_axis, x_axis), -x_axis])
 
-        # rotation for u and v direction IA_T_AA
+        # rotation for u and v direction
         axis_align_R = np.eye(4, 4)
         axis_align_R[0:3, 0] = u_dir
         axis_align_R[0:3, 1] = v_dir
         axis_align_R[0:3, 2] = np.cross(u_dir, v_dir)
         axis_align_R = axis_align_R.T
 
-        # rotation about x axis AA_T_PLANE
+        # rotation about x axis
         R_x_axis = np.eye(4, 4)
         R_x_axis[0:3, 0:3] = np.array([1,                      0,                      0,
                                        0,  np.cos(-current_angle), -np.sin(-current_angle),
                                        0,  np.sin(-current_angle),  np.cos(-current_angle)]).reshape((3, 3))
 
-        # translation CAM_T_IA
+        # translation of camera
         translation = np.eye(4, 4)
         translation[0:4, 3] = np.array([0, 0, geometry.source_isocenter_distance, 1])
 
@@ -147,21 +134,12 @@ def circular_trajectory_3d(geometry):
         extrinsic_params_mat = np.dot(np.dot(np.dot(translation, axis_align_R), R_x_axis), R_to_plane)
         extrinsic_params_mat = extrinsic_params_mat / extrinsic_params_mat[3, 3]
 
-        diff_extrinsic = geometry.conrad_Rt[p] - extrinsic_params_mat
-        #print("diff_extrinsic\n", np.sum(diff_extrinsic))
-        #print("diff_extrinsic\n", diff_extrinsic)
-        #print("My Matrix Rt:", p ," \n", extrinsic_params_mat)
-
         # calculate projection matrix
         projection_matrices[p][0:3, 0:3] = np.dot(intrinsic_params_mat, extrinsic_params_mat[0:3, 0:3])
         projection_matrices[p][0:3, 3] = np.dot(intrinsic_params_mat, extrinsic_params_mat[0:3, 3])
 
         # next angle
         current_angle += angular_increment
-
-        #print('my mat: \n', projection_matrices[p])
-        #print('conrad mat: \n', geometry.projection_matrices[p])
-        #print('diff: \n', projection_matrices[p]-geometry.projection_matrices[p])
 
     return projection_matrices
 
@@ -176,12 +154,12 @@ if __name__ == '__main__':
         volume_size = int(np.random.uniform(10, 1000))
         volume_shape = [1*volume_size, 2*volume_size, 3*volume_size]
         volume_spacing = [0.5, 0.5, 0.5]
-        #volume_spacing = np.random.uniform(0.1, 1, 3)
+        volume_spacing = np.random.uniform(0.1, 1, 3)
 
         # Detector Parameters:
         detector_shape = [4*volume_size, 5*volume_size]
         detector_spacing = [0.5, 0.5]
-        #detector_spacing = np.random.uniform(0.1, 1, 2)
+        detector_spacing = np.random.uniform(0.1, 1, 2)
 
         # Trajectory Parameters:
         number_of_projections = int(np.random.uniform(10, 1000))
@@ -194,12 +172,7 @@ if __name__ == '__main__':
         geometry = GeometryCone3D(volume_shape, volume_spacing, detector_shape, detector_spacing, number_of_projections, angular_range, source_detector_distance, source_isocenter_distance)
 
         # test vs pyconrad
-        conrad_projection_matrices, conrad_Rt, conrad_intrinsic = circular_trajectory_3d_pyconrad(geometry)
-        geometry.set_projection_matrices(conrad_projection_matrices)
-
-        geometry.conrad_Rt = conrad_Rt
-        geometry.conrad_intrinsic = conrad_intrinsic
-
+        conrad_projection_matrices = circular_trajectory_3d_pyconrad(geometry)
         matrices = circular_trajectory_3d(geometry)
 
         overall_diff = conrad_projection_matrices-matrices
