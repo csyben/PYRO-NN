0c0f0a17db8d7c40d878d3e80c2545113f5a5882
syben <christopher.syben@fau.de>
2019-02-19 14:35:17 +0100

    added ramp and ramlak in a 3D version which can direclty be used in a tf.multiply layer for filtering the fourier transformed sinogram

--- deep_ct_reconstruction/ct_reconstruction/helpers/filters/filters.py
+++ deep_ct_reconstruction/ct_reconstruction/helpers/filters/filters.py
@@ -12,7 +12,7 @@ def ramp(detector_width):
             filter_array[i] = i * frequency_spacing
         elif i > filter_array.shape[0] / 2.0:
             filter_array[i] = 0.5 - (((i - filter_array.shape[0]/2.0))*frequency_spacing)
-    return filter_array
+    return filter_array.astype(np.float32)
 
 def ram_lak(detector_width, detector_spacing):
     # TODO: Roll scaling to 0.25
@@ -30,4 +30,35 @@ def ram_lak(detector_width, detector_spacing):
                 filter_array[i] = odd / (tmp * tmp)
 
     filter_array = np.fft.fft(filter_array)
-    return np.real(filter_array)
+    return np.real(filter_array).astype(np.float32)
+
+def ramp_3D(geometry):
+    detector_width = geometry.detector_shape[np.alen(geometry.detector_shape) - 1]
+
+    filter = [
+        np.reshape(
+            ramp(detector_width),
+            (1, 1, detector_width)
+        )
+        for i in range(0, geometry.number_of_projections)
+    ]
+
+    filter = np.concatenate(filter)
+
+    return filter
+
+def ram_lak_3D(geometry):
+    detector_width = geometry.detector_shape[np.alen(geometry.detector_shape) - 1]
+    detector_spacing_width = geometry.detector_spacing[np.alen(geometry.detector_spacing) - 1]
+
+    filter = [
+        np.reshape(
+            ram_lak(detector_width, detector_spacing_width),
+            (1, 1, detector_width)
+        )
+        for i in range(0, geometry.number_of_projections)
+    ]
+
+    filter = np.concatenate(filter)
+
+    return filter
