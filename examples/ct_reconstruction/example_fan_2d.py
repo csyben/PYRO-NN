ed4c96a10cb0dcd9dd6a3d094fccfbf01737f2d8
syben <christopher.syben@fau.de>
2019-03-04 16:15:39 +0100

    insert fan-beam test. This example throws an error with the old layers (Bug: tensor_proto_central_ray not found in geometry). With current workaround example runs. Projectorpair is not yet scaled perfectly, distance weighting is not correct in the CUDA kernel yet

--- examples/ct_reconstruction/example_fan_2d.py
+++ examples/ct_reconstruction/example_fan_2d.py
@@ -10,26 +10,27 @@ from deep_ct_reconstruction.ct_reconstruction.layers.backprojection_2d import fa
 from deep_ct_reconstruction.ct_reconstruction.geometry.geometry_fan_2d import GeometryFan2D
 from deep_ct_reconstruction.ct_reconstruction.helpers.phantoms import shepp_logan
 from deep_ct_reconstruction.ct_reconstruction.helpers.trajectories import circular_trajectory
+from deep_ct_reconstruction.ct_reconstruction.helpers.filters import filters
 
 
 def example_fan_2d():
     # ------------------ Declare Parameters ------------------
 
     # Volume Parameters:
-    volume_size = 200
+    volume_size = 512
     volume_shape = [volume_size, volume_size]
     volume_spacing = [0.5, 0.5]
 
     # Detector Parameters:
-    detector_shape = 2*volume_size
+    detector_shape = 800
     detector_spacing = 0.5
 
     # Trajectory Parameters:
-    number_of_projections = 100
-    angular_range = np.pi
+    number_of_projections = 360
+    angular_range = 2*np.pi
 
-    source_detector_distance = 600
-    source_isocenter_distance = 600
+    source_detector_distance = 1200
+    source_isocenter_distance = 750
 
     # create Geometry class
     geometry = GeometryFan2D(volume_shape, volume_spacing, detector_shape, detector_spacing, number_of_projections, angular_range, source_detector_distance, source_isocenter_distance)
@@ -46,7 +47,13 @@ def example_fan_2d():
         sinogram = result.eval()
         pyc.imshow(sinogram, 'sinogram')
 
-        result_back_proj = fan_backprojection2d(sinogram, geometry)
+        reco_filter = filters.ram_lak_2D(geometry)
+
+        sino_freq = np.fft.fft(sinogram, axis=1)
+        sino_filtered_freq = np.multiply(sino_freq, reco_filter)
+        sinogram_filtered = np.fft.ifft(sino_filtered_freq, axis=1)
+
+        result_back_proj = fan_backprojection2d(sinogram_filtered, geometry)
         reco = result_back_proj.eval()
         pyc.imshow(reco, 'reco')
 
