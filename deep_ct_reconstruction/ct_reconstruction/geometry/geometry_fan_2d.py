ed67d8ccfd437f89895b3bc929aef0565042ed0a
Markus Michen <markus.michen@fau.de>
2019-03-05 11:36:37 +0100

    [fixed] - geometry_fan2d: central_ray_vector member getter/setter for tensor proto member

--- deep_ct_reconstruction/ct_reconstruction/geometry/geometry_fan_2d.py
+++ deep_ct_reconstruction/ct_reconstruction/geometry/geometry_fan_2d.py
@@ -32,6 +32,6 @@ class GeometryFan2D(GeometryBase):
         self.central_ray_vectors = np.array(central_ray_vectors, self.np_dtype)
 
     @GeometryBase.SetTensorProtoProperty
-    def ray_vectors(self, value):
+    def central_ray_vectors(self, value):
         self.__dict__['central_ray_vectors'] = value
         self.tensor_proto_central_ray_vectors = super().to_tensor_proto(self.central_ray_vectors)
