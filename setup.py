1266a86f873667e82e3514abbd0d25fb49e93153
syben <christopher.syben@fau.de>
2019-03-06 17:06:17 +0100

    setup file to built pip package -- need valid framework name

---
+++ setup.py
@@ -0,0 +1,40 @@
+__author__ = "Christopher Syben <christopher.syben@fau.de>"
+__copyright__ = "Christopher Syben <christopher.syben@fau.de>"
+__license__ = """
+deeptomo, python framework for convenient use of the ct reconstructions algorithms provided within Tensorflow
+Copyright (C) 2017 Christopher Syben <christopher.syben@fau.de>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+import setuptools
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="deep_ct_reconstruction",
+    version="0.0.1",
+    author="Christopher Syben",
+    author_email="christopher.syben@fau.de",
+    description="grml-tk is the high level Python API to the deeptomo known operators.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/grml-tk",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
