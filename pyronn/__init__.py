64f8484c5c821b2a08f4a1f46a0a899728b8058c
syben <christopher.syben@fau.de>
2019-03-11 17:34:25 +0100

    changed import to pyronn_layers

--- pyronn/__init__.py
+++ pyronn/__init__.py
@@ -1,5 +1,5 @@
 try:
-    import lme_custom_ops
+    import pyronn_layers
 except ImportError:
     import warnings
     warnings.warn('Could not import pyRO-NN-Layers. Please install pyRO-NN-Layers from www.github.com/csyben/pyRO-NN-Layers')
