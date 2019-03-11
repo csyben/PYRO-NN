5d7236750d2e7483943fef87bd0b4e1af06abef7
syben <christopher.syben@fau.de>
2019-03-11 15:33:42 +0100

    add __init__ file. Test import of layer with Warning if custom ops are not available.

---
+++ pyronn/__init__.py
@@ -0,0 +1,7 @@
+try:
+    import lme_custom_ops
+except ImportError:
+    import warnings
+    warnings.warn('Could not import pyRO-NN-Layers. Please install pyRO-NN-Layers from www.github.com/csyben/pyRO-NN-Layers')
+
+name = "pyronn"
