[fixed] - correct calculation of fan and cone angles

pyronn/ct_reconstruction/geometry/geometry_cone_3d.py
@@ -19,10 +19,9 @@ class GeometryCone3D(GeometryBase):
                          source_detector_distance, source_isocenter_distance)
 
         # defined by geometry so calculate for convenience use
-        self.fan_angle = np.arctan(
-            self.detector_shape[1] * self.detector_spacing[1] / 2.0 / self.source_detector_distance)
-        self.cone_angle = np.arctan(
-            self.detector_shape[0] * self.detector_spacing[0] / 2.0 / self.source_detector_distance)
+        self.fan_angle  = np.arctan(((self.detector_shape[1] - 1) / 2.0 * self.detector_spacing[1]) / self.source_detector_distance)
+        self.cone_angle = np.arctan(((self.detector_shape[0] - 1) / 2.0 * self.detector_spacing[0]) / self.source_detector_distance)
+
         # Containing the constant part of the distance weight and discretization invariant
         self.projection_multiplier = self.source_isocenter_distance * self.source_detector_distance * detector_spacing[-1] * np.pi / self.number_of_projections
 

pyronn/ct_reconstruction/geometry/geometry_fan_2d.py
@@ -19,8 +19,7 @@ class GeometryFan2D(GeometryBase):
                          source_detector_distance, source_isocenter_distance)
 
         # defined by geometry so calculate for convenience use
-        self.fan_angle = np.arctan(
-            self.detector_shape[0] * self.detector_spacing[0] / 2.0 / self.source_detector_distance)
+        self.fan_angle = np.arctan(((self.detector_shape[0] - 1) / 2.0 * self.detector_spacing[0]) / self.source_detector_distance)
 
     def set_central_ray_vectors(self, central_ray_vectors):
         """