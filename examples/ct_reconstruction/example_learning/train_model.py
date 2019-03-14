22330ca6b6308d56cf49d1c42ba4b15397b01f52
Markus Michen <markus.michen@fau.de>
2019-03-14 22:35:09 +0100

    [modified] - now using eval model function and batchsizes - added results holder class for results

--- examples/ct_reconstruction/example_learning/train_model.py
+++ examples/ct_reconstruction/example_learning/train_model.py
@@ -1,3 +1,4 @@
+import numpy as np
 import tensorflow as tf
 import os.path
 from examples.ct_reconstruction.example_learning.model.geometry_parameters import GEOMETRY
@@ -5,21 +6,23 @@ from examples.ct_reconstruction.example_learning.model import model, input_data
 
 # training parameters
 LEARNING_RATE          = 0.0001
-NUM_TRAINING_SAMPLES   = 100
-NUM_VALIDATION_SAMPLES = 50
+BATCH_SIZE_TRAIN       = 10
+NUM_TRAINING_SAMPLES   = BATCH_SIZE_TRAIN * 10
+MAX_TRAIN_STEPS        = NUM_TRAINING_SAMPLES//BATCH_SIZE_TRAIN
+BATCH_SIZE_VALIDATION  = 10
+NUM_VALIDATION_SAMPLES = BATCH_SIZE_VALIDATION*10
+MAX_VALIDATION_STEPS   = NUM_VALIDATION_SAMPLES//BATCH_SIZE_VALIDATION
 NUM_TEST_SAMPLES       = 1
-MAX_TRAIN_STEPS        = NUM_TRAINING_SAMPLES
-MAX_VALID_STEPS        = NUM_VALIDATION_SAMPLES
 MAX_TEST_STEPS         = NUM_TEST_SAMPLES
-MAX_EPOCHS             = 5
-
-import pyconrad as pyc
-import time
-pyc.setup_pyconrad()
-pyc.start_gui()
+MAX_EPOCHS             = 10
 
 
 class Pipeline:
+
+
+    # Class as namespace for storing results
+    class Results:
+        pass
 
 
     def __init__(self):
@@ -36,14 +39,15 @@ class Pipeline:
         self.labels_placeholder = tf.placeholder(tf.float32, shape=(None,) + tuple(GEOMETRY.volume_shape))
 
         # create tf dataset
-        dataset = tf.data.Dataset.from_tensor_slices((self.data_placeholder, self.labels_placeholder)).batch(1).repeat()
+        dataset = tf.data.Dataset.from_tensor_slices((self.data_placeholder, self.labels_placeholder))\
+            .batch(self.batch_size_placeholder).repeat()
 
         # create a iterator
         self.iter = dataset.make_initializable_iterator()
-        self.images, self.labels = self.iter.get_next()
+        self.sinograms, self.labels = self.iter.get_next()
 
         # call Model
-        self.backprojection_layer = self.model.forward(self.images)
+        self.backprojection_layer = self.model.forward(self.sinograms)
         self.loss                 = self.model.l2_loss(self.backprojection_layer, self.labels)
         self.train_op             = self.model.training_op(self.loss, LEARNING_RATE)
 
@@ -54,23 +58,27 @@ class Pipeline:
         self.saver = tf.train.Saver()
 
     
-    def do_model_eval(self, sess, input, labels, batch_size):
-        # init with data
+    def do_model_eval(self, sess, input, labels, batch_size, steps):
+
+        # initialize iterator with data
         sess.run(self.iter.initializer, feed_dict={self.data_placeholder: input,
                                                        self.labels_placeholder: labels,
-                                                       self.batch_size_placeholder: 1})
-        for i in range(batch_size):
+                                                       self.batch_size_placeholder: batch_size})
+        # run model and calc avg loss for set
+        avg_loss = 0
+        for i in range(steps):
             reco, loss_value = sess.run ([self.backprojection_layer, self.loss])
+            avg_loss +=loss_value
 
-        return reco, loss_value
+        return reco, loss_value, avg_loss/steps
 
 
     def run_training(self):
         
         # get data
-        train_data_numpy, train_labels_numpy = input_data.generate_training_data(NUM_TRAINING_SAMPLES, 0.05)
+        train_data_numpy, train_labels_numpy           = input_data.generate_training_data  (NUM_TRAINING_SAMPLES, 0.00)
         validation_data_numpy, validation_labels_numpy = input_data.generate_validation_data(NUM_VALIDATION_SAMPLES)
-        test_data_numpy, test_labels_numpy = input_data.get_test_data(NUM_TEST_SAMPLES)
+        test_data_numpy, test_labels_numpy             = input_data.get_test_data           (NUM_TEST_SAMPLES)
 
         # session
         config = tf.ConfigProto()
@@ -78,27 +86,27 @@ class Pipeline:
         config.gpu_options.allow_growth = True
         with tf.Session(config=config) as sess:
 
+            # Build Graph
             self.build_graph()
             sess.run(tf.global_variables_initializer())
             sess.run(tf.local_variables_initializer())
-            pyc.imshow(np.fft.fftshift(self.model.filter_weights.eval()), 'inital')
+
+            # Save initial filter for results
+            self.Results.initial_filter = np.fft.fftshift(self.model.filter_weights.eval())
 
             for epoch in range(MAX_EPOCHS):
                 print('EPOCH: ', epoch)
                 # initialise iterator with train data
                 sess.run(self.iter.initializer, feed_dict={self.data_placeholder: train_data_numpy,
                                                            self.labels_placeholder: train_labels_numpy,
-                                                           self.batch_size_placeholder: NUM_TRAINING_SAMPLES})
+                                                           self.batch_size_placeholder: BATCH_SIZE_TRAIN})
                 
                 for step in range(MAX_TRAIN_STEPS):
                     # get next batch
-                    _, training_loss_value, train_reco = sess.run([self.train_op, self.loss, self.backprojection_layer])
+                    _, training_loss_value = sess.run([self.train_op, self.loss])
 
-                    #pyc.imshow(train_reco, 'current_train_reco')
-                    #time.sleep(1)
-
-                    # print loss
-                    if(step % 10 == 0):
+                    # print loss after each batch
+                    if(step % BATCH_SIZE_TRAIN == 0):
                         print('training_loss: ', training_loss_value)
                         sum = sess.run(self.summary)
                         self.writer.add_summary(sum, (epoch * MAX_TRAIN_STEPS) + step)
@@ -110,33 +118,26 @@ class Pipeline:
 
                 # Every finished epoch validation
                 print('Evaluation on Validation data:')
-                sess.run(self.iter.initializer, feed_dict={self.data_placeholder: validation_data_numpy,
-                                                           self.labels_placeholder: validation_labels_numpy,
-                                                           self.batch_size_placeholder: 1})
-                for step in range(MAX_VALID_STEPS):
-                    validation_reco, validation_loss_value = sess.run([self.backprojection_layer, self.loss])
-                    #pyc.imshow(validation_reco, 'validation_reco')
-                    #time.sleep(1)
-                    print('validation_loss: ', validation_loss_value)
+                _, _, avg_validation_loss_value = self.do_model_eval(sess, validation_data_numpy, validation_labels_numpy,
+                                                                     BATCH_SIZE_VALIDATION, MAX_VALIDATION_STEPS)
+                print('avg_validation_loss: ', avg_validation_loss_value)
 
             # Finished training, eval on test set
             print('-----------------------------------------------')
             print('Run trained model on test data: ')
-            sess.run(self.iter.initializer, feed_dict={self.data_placeholder: test_data_numpy,
-                                                           self.labels_placeholder: test_labels_numpy,
-                                                           self.batch_size_placeholder: 1})
-            for step in range(MAX_TEST_STEPS):
-                test_reco, test_loss_value = sess.run([self.backprojection_layer, self.loss])
-                pyc.imshow(test_reco, 'test_reco')
-                #time.sleep(1)
-                print('test_loss: ', test_loss_value)
-            import numpy as np
+            self.Results.test_reco, self.Results.test_loss, _ = self.do_model_eval(sess, test_data_numpy, test_labels_numpy,
+                                                                                   NUM_TEST_SAMPLES, MAX_TEST_STEPS)
+            print('test_loss: ', self.Results.test_loss)
+            self.Results.learned_filter = np.fft.fftshift(self.model.filter_weights.eval())
 
-            pyc.imshow(np.fft.fftshift(self.model.filter_weights.eval()), 'learned')
 
+def plot_results(results):
+    pass
 
             
 if __name__ == '__main__':
     p = Pipeline()
     p.run_training()
+    plot_results(p.Results)
+
 
