f56c1a1453e1cc9ebcf651a0016896f8cb268f50
syben <christopher.syben@fau.de>
2019-03-15 16:57:38 +0100

    save plot with pyplot. Remove plot function

--- examples/ct_reconstruction/example_fdk.py
+++ examples/ct_reconstruction/example_fdk.py
@@ -1,11 +1,7 @@
 import numpy as np
 import tensorflow as tf
-import math
+import matplotlib.pyplot as plt
 
-import sys
-sys.path.append('/home/markus/FAU/Project/deep_ct_reconstruction_master')
-
-# TODO: better imports
 from pyronn.ct_reconstruction.helpers.misc import generate_sinogram
 from pyronn.ct_reconstruction.layers.projection_3d import cone_projection3d
 from pyronn.ct_reconstruction.layers.backprojection_3d import cone_backprojection3d
@@ -13,66 +9,9 @@ from pyronn.ct_reconstruction.geometry.geometry_cone_3d import GeometryCone3D
 from pyronn.ct_reconstruction.helpers.phantoms import shepp_logan
 from pyronn.ct_reconstruction.helpers.trajectories import circular_trajectory
 from pyronn.ct_reconstruction.helpers.filters.filters import ram_lak_3D
-
 import pyronn.ct_reconstruction.helpers.filters.weights as ct_weights
 
-import pyconrad as pyc
-pyc.setup_pyconrad()
-pyc.start_gui()
 
-
-def medphys_plot():
-    import matplotlib.pyplot as plt
-    path = '/home/syben/Documents/medphys/'
-    _ = pyc.ClassGetter()
-    phantom = pyc.PyGrid.from_tiff(path+'phantom.tif')
-    custom_ops = pyc.PyGrid.from_tiff(path + 'short_fdk_custom_ops_reco.tif')
-    conrad = pyc.PyGrid.from_tiff(path + 'short_fdk_conrad_reco.tif')
-
-    slice = 128
-    row = 128
-
-    phantom_central = np.array(phantom[slice, :, :])
-    custom_ops_central = np.array(custom_ops[slice, :, :])
-    conrad_central = np.array(conrad[slice, :, :])
-
-    gt = phantom_central[row,:]
-    p1 = custom_ops_central[row,:]
-    p2 = conrad_central[row,:]
-
-    fig = plt.figure(figsize=(30, 10))
-    number_of_collumns = 6
-    number_of_rows = 3
-    img_one = plt.subplot2grid((number_of_rows, number_of_collumns), (0, 0), colspan=2,rowspan=3)
-
-    plot_one = plt.subplot2grid((number_of_rows, number_of_collumns), (0, 2), colspan=4, rowspan=3)  # , sharex=img_one)
-
-
-    img_width = np.shape(phantom_central)[0]
-    img_one.plot((0, img_width), (row, row), '--', linewidth=5)
-
-    img_one.imshow(phantom_central, cmap=plt.get_cmap('gist_gray'), vmin=0, vmax=0.4)
-    img_one.axis('off')
-    img_one.set_title('Central Slice NN Reconstruction', fontsize=35, y=1.05)
-
-
-    plot_one.plot(np.arange(len(gt)), gt, color='#1f77b4')
-    plot_one.plot(np.arange(len(p1)), p1, color='lightgreen')
-    plot_one.plot(np.arange(len(p2)), p2, linestyle=':', color='red')
-
-    min_plt = 0
-    max_plt = 1.1
-
-    plot_one.set_ylim(plt.ylim((min_plt, max_plt)))
-    plot_one.legend(['Shepp-Logan Phantom', 'NN Reconstruction', 'CONRAD Reconstruction'], loc='upper center', prop={'size': 35})
-    #plt.savefig('evaluation/eval_img/' + experiment_img_name + '.png', dpi=150, transparent=False, bbox_inches='tight')
-    plt.gcf().tight_layout(h_pad=0, w_pad=0)
-
-    plt.savefig(path + 'fdk_eval.png', dpi=150, transparent=False, bbox_inches='tight')
-
-    fig.show()
-
-    a = 5
 
 class nn_model:
     def __init__(self, geometry):
@@ -80,8 +19,6 @@ class nn_model:
 
         self.cosine_weight = tf.get_variable(name='cosine_weight', dtype=tf.float32,
                                              initializer=ct_weights.cosine_weights_3d(self.geometry), trainable=False)
-
-        primary_angles_2 = np.linspace(0, geometry.angular_range, geometry.number_of_projections)
 
         self.redundancy_weight = tf.get_variable(name='redundancy_weight', dtype=tf.float32,
                                              initializer=ct_weights.parker_weights_3d(self.geometry), trainable=False)
@@ -107,13 +44,13 @@ def example_cone_3d():
     # ------------------ Declare Parameters ------------------
 
     # Volume Parameters:
-    volume_size = 151
+    volume_size = 256
     volume_shape = [volume_size, volume_size, volume_size]
     v_spacing = 0.25
     volume_spacing = [v_spacing,v_spacing,v_spacing]
 
     # Detector Parameters:
-    detector_shape = [251 , 251]
+    detector_shape = [450 , 450]
     d_spacing = 0.33
     detector_spacing = [d_spacing,d_spacing]
 
@@ -126,15 +63,13 @@ def example_cone_3d():
 
     # create Geometry class
     geometry = GeometryCone3D(volume_shape, volume_spacing, detector_shape, detector_spacing, number_of_projections, angular_range, source_detector_distance, source_isocenter_distance)
-    geometry.angular_range = np.radians(200) #np.pi + 2*geometry.fan_angle
+    geometry.angular_range = np.radians(200)
     projection_geometry = circular_trajectory.circular_trajectory_3d(geometry)
 
     geometry.set_projection_matrices(projection_geometry)
-    #geometry.set_projection_matrices(circular_trajectory.circular_trajectory_3d(geometry))
-    #print(geometry.projection_matrices[0])
+
     # Get Phantom 3d
     phantom = shepp_logan.shepp_logan_3d(volume_shape)
-    pyc.imshow(phantom, 'phantom')
 
 
 
@@ -144,20 +79,18 @@ def example_cone_3d():
     # ------------------ Call Layers ------------------
     with tf.Session(config=config) as sess:
         sinogram = generate_sinogram.generate_sinogram(phantom, cone_projection3d, geometry)
-        pyc.imshow(sinogram, 'sinogram')
 
         model = nn_model(geometry)
         sess.run(tf.global_variables_initializer())
         sess.run(tf.local_variables_initializer())
         reco_tf, redundancy_weighted_sino_tf = model.model(sinogram)
         reco, redundancy_weighted_sino = sess.run([reco_tf, redundancy_weighted_sino_tf])
-    pyc.imshow(reco,'reco')
-    #pyc.imshow(redundancy_weighted_sino, 'redundancy_weighted_sino')
-    a=5
 
-
+    plt.figure()
+    plt.imshow(reco[(int)(volume_shape[0]/2),:,:], cmap=plt.get_cmap('gist_gray'), vmin=0, vmax=0.4)
+    plt.axis('off')
+    plt.savefig('fdk_reco.png', dpi=150, transparent=False, bbox_inches='tight')
 
 
 if __name__ == '__main__':
-    #medphys_plot()
     example_cone_3d()
