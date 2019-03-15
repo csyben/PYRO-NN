900dc82f72e3c8bfc4a5d850f70e700f42f78734
Markus Michen <markus.michen@fau.de>
2019-03-15 22:47:55 +0100

    [modified] - changed ordering of functions

--- pyronn/ct_reconstruction/helpers/filters/filters.py
+++ pyronn/ct_reconstruction/helpers/filters/filters.py
@@ -1,5 +1,4 @@
 import numpy as np
-from math import pi
 
 
 def ramp(detector_width):
@@ -11,24 +10,6 @@ def ramp(detector_width):
         elif i > filter_array.shape[0] / 2.0:
             filter_array[i] = 0.5 - (((i - filter_array.shape[0] / 2.0)) * frequency_spacing)
     return filter_array.astype(np.float32)
-
-
-def ram_lak(detector_width, detector_spacing):
-    filter_array = np.zeros(detector_width)
-    filter_array[0] = (0.25 / (detector_spacing * detector_spacing))
-    odd = (-1.0 / (pi * pi * detector_spacing * detector_spacing))
-
-    for i in range(1, int(filter_array.shape[0])):
-        if i < filter_array.shape[0] / 2:
-            if (i % 2) == 1:
-                filter_array[i] = odd / (i * i)
-        if i >= filter_array.shape[0] / 2:
-            tmp = filter_array.shape[0] - i
-            if (tmp % 2) == 1:
-                filter_array[i] = odd / (tmp * tmp)
-
-    filter_array = np.fft.fft(filter_array)
-    return np.real(filter_array).astype(np.float32)
 
 
 def ramp_2D(geometry):
@@ -61,6 +42,24 @@ def ramp_3D(geometry):
     filter = np.concatenate(filter)
 
     return filter
+
+
+def ram_lak(detector_width, detector_spacing):
+    filter_array = np.zeros(detector_width)
+    filter_array[0] = (0.25 / (detector_spacing * detector_spacing))
+    odd = (-1.0 / (np.pi * np.pi * detector_spacing * detector_spacing))
+
+    for i in range(1, int(filter_array.shape[0])):
+        if i < filter_array.shape[0] / 2:
+            if (i % 2) == 1:
+                filter_array[i] = odd / (i * i)
+        if i >= filter_array.shape[0] / 2:
+            tmp = filter_array.shape[0] - i
+            if (tmp % 2) == 1:
+                filter_array[i] = odd / (tmp * tmp)
+
+    filter_array = np.fft.fft(filter_array)
+    return np.real(filter_array).astype(np.float32)    
 
 
 def ram_lak_2D(geometry):
