e86666400030148d2063118f4c44bb05585fdbe8
syben <christopher.syben@fau.de>
2019-04-29 18:24:10 +0200

    final license type: Apache 2.0

--- setup.py
+++ setup.py
@@ -2,20 +2,19 @@ __author__ = "Christopher Syben <christopher.syben@fau.de>"
 __copyright__ = "Christopher Syben <christopher.syben@fau.de>"
 __license__ = """
 PYRO-NN, python framework for convenient use of the ct reconstructions algorithms provided within Tensorflow
-Copyright (C) 2019 Christopher Syben <christopher.syben@fau.de>
+Copyright [2019] [Christopher Syben]
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+ http://www.apache.org/licenses/LICENSE-2.0
 
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 """
 import setuptools
 
@@ -24,7 +23,7 @@ with open("README.rst", "r") as fh:
 
 setuptools.setup(
     name="pyronn",
-    version="0.0.5",
+    version="0.0.6",
     author="Christopher Syben",
     author_email="christopher.syben@fau.de",
     description="PYRO-NN is the high level Python API to the PYRO-NN-Layers known operators.",
@@ -34,7 +33,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "License :: OSI Approved :: Apache License 2.0",
         "Operating System :: OS Independent",
     ],
 )
