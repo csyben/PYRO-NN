b29b9597ce9ec8f2dc3c53c293292b0f0be93659
syben <christopher.syben@fau.de>
2019-05-13 15:42:43 +0200

    add 3d parallel projector and backprojector layers with registered gradients

--- pyronn/ct_reconstruction/layers/backprojection_3d.py
+++ pyronn/ct_reconstruction/layers/backprojection_3d.py
@@ -55,3 +55,43 @@ def _backproject_grad(op, grad):
         projection_multiplier=op.get_attr("projection_multiplier")
     )
     return [proj]
+
+
+
+# parallel_backprojection3d
+def par_backprojection3d(sinogram, geometry, hardware_interp=True):
+    """
+    Wrapper function for making the layer call.
+    Args:
+        volume:             Input volume to project.
+        geometry:           Corresponding GeometryCone3D Object defining parameters.
+        hardware_interp:    Controls if interpolation is done by GPU
+    Returns:
+            Initialized lme_custom_ops.cone_backprojection3d layer.
+    """
+    return pyronn_layers.parallel_backprojection3d(sinogram,
+                                                sinogram_shape=geometry.sinogram_shape,
+                                                volume_shape=geometry.volume_shape,
+                                                volume_origin=geometry.tensor_proto_volume_origin,
+                                                volume_spacing=geometry.tensor_proto_volume_spacing,
+                                                detector_origin=geometry.tensor_proto_detector_origin,
+                                                detector_spacing=geometry.tensor_proto_detector_spacing,
+                                                ray_vectors=geometry.tensor_proto_ray_vectors)
+
+
+@ops.RegisterGradient("ParallelBackprojection3D")
+def _backproject_grad(op, grad):
+    '''
+        Compute the gradient of the backprojector op by invoking the forward projector.
+    '''
+    proj = pyronn_layers.parallel_projection3d(
+        volume=grad,
+        volume_shape=op.get_attr("volume_shape"),
+        projection_shape=op.get_attr("sinogram_shape"),
+        volume_origin=op.get_attr("volume_origin"),
+        volume_spacing=op.get_attr("volume_spacing"),
+        detector_origin=op.get_attr("detector_origin"),
+        detector_spacing=op.get_attr("detector_spacing"),
+        ray_vectors=op.get_attr("projection_matrices"),
+    )
+    return [proj]
--- pyronn/ct_reconstruction/layers/projection_3d.py
+++ pyronn/ct_reconstruction/layers/projection_3d.py
@@ -57,3 +57,47 @@ def _project_grad(op, grad):
         step_size=op.get_attr("step_size")
     )
     return [reco]
+
+
+# par_projection3d
+def par_projection3d(volume, geometry, hardware_interp=True, step_size=1.0):
+    """
+    Wrapper function for making the layer call.
+    Args:
+        volume:             Input volume to project.
+        geometry:           Corresponding GeometryCone3D Object defining parameters.
+        hardware_interp:    Controls if interpolation is done by GPU.
+        step_size:          step_size along ray direction in voxel.
+    Returns:
+            Initialized lme_custom_ops.cone_projection3d layer.
+    """
+
+    return pyronn_layers.parallel_projection3d(volume,
+                                            volume_shape=geometry.volume_shape,
+                                            projection_shape=geometry.sinogram_shape,
+                                            volume_origin=geometry.tensor_proto_volume_origin,
+                                            detector_origin=geometry.tensor_proto_detector_origin,
+                                            volume_spacing=geometry.tensor_proto_volume_spacing,
+                                            detector_spacing=geometry.tensor_proto_detector_spacing,
+                                            ray_vectors=geometry.tensor_proto_ray_vectors)
+
+
+
+@ops.RegisterGradient("ParallelProjection3D")
+def _project_grad(op, grad):
+    '''
+    Compute the gradient of the projection op by invoking the backprojector.
+    '''
+    reco = pyronn_layers.parallel_backprojection3d(
+        sinogram=grad,
+        sinogram_shape=op.get_attr("projection_shape"),
+        volume_shape=op.get_attr("volume_shape"),
+        volume_origin=op.get_attr("volume_origin"),
+        volume_spacing=op.get_attr("volume_spacing"),
+        detector_origin=op.get_attr("detector_origin"),
+        detector_spacing=op.get_attr("detector_spacing"),
+        ray_vectors=op.get_attr("ray_vectors"),
+    )
+
+
+    return [reco]
