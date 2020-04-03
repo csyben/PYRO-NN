2fd069746b1692b3fdb3caed8ac6d166ba3e372b
syben <christopher.syben@fau.de>
2020-04-03 23:44:50 +0200

    pyronn will automatically set tf to allow memory growth.

--- pyronn/__init__.py
+++ pyronn/__init__.py
@@ -14,6 +14,18 @@
 
 try:
     import pyronn_layers
+    import tensorflow as tf
+    try:
+        print("Tensorflow: memory_growth is set to True")
+        gpus = tf.config.experimental.list_physical_devices('GPU')
+        if gpus:
+            try:
+                for gpu in gpus:
+                    tf.config.experimental.set_memory_growth(gpu, True)
+            except RunetimeError as e:
+                print(e)
+    except error as e:
+        print("Failed to allow memory growth for Tensorflow. Be aware of memory limitations using the hardware interpolate flag for the PYRO-NN layers.")
 except ImportError:
     import warnings
     warnings.warn('Could not import PYRO-NN-Layers. Please install PYRO-NN-Layers from www.github.com/csyben/pyRO-NN-Layers')
